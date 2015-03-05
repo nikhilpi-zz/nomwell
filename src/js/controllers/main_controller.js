55dc950835dc3b10fa8088e9042ceec50fe6fe70
Brad Puder <bradleypuder2015@u.northwestern.edu>
2015-03-04 20:38:38 -0600

    added a variety of dishes

--- src/js/controllers/main_controller.js
+++ src/js/controllers/main_controller.js
@@ -4,29 +4,73 @@ angular.module('NomWell.controllers.Main', [])
   $scope.user = {
 
   };
+
   $scope.foods = [
     {
-      name: 'Quinoa',
-      description: 'Healthy Food'
+      name: 'Quinoa Salad',
+      description: 'Healthy Food',
+      price: 10,
+      vendor: 'Lyfe Kitchen',
+      eta: '30',
+      img: 'http://www.kraftrecipes.com/assets/recipe_images/Mediterranean-Quinoa-salad-57818.jpg',
+      favorite: false,
+      ingredients: ['quinoa', 'lemon', 'mint', 'squash'],
+      fat: 3,
+      calories: 300,
+      salt: 3,
+      vegetarian: false,
+      thai: false,
+      italian: false
     },
     {
-      name: 'Kale',
-      description: 'Kale me'
+      name: 'Caprese Pasta',
+      description: 'Healthy Food',
+      price: 12,
+      vendor: 'Bravo',
+      eta: '35',
+      img: 'http://www.ilovepasta.org/sites/default/files/feature-bg-imgs/pasta-slide-001_0.jpg',
+      favorite: false,
+      ingredients: ['tomato', 'basil', 'farfalle', 'spinach'],
+      fat: 4,
+      calories: 500,
+      salt: 2,
+      vegetarian: true,
+      thai: false,
+      italian: true
+    },
+    {
+      name: 'Grilled Tilapia',
+      description: 'Healthy Food',
+      price: 13,
+      vendor: 'Lyfe Kitchen',
+      eta: '30',
+      img: 'http://www.muscleprodigy.com/content/articles/home/feb-12-2011-meal-plan-of-the-day-1344.jpg',
+      favorite: false,
+      ingredients: ['tilapia', 'green beans', 'tomato', 'onions'],
+      fat: 3,
+      calories: 300,
+      salt: 3,
+      vegetarian: false,
+      thai: false,
+      italian: true
+    },
+    {
+      name: 'Lettuce Wraps',
+      description: 'Healthy Food',
+      price: 8,
+      vendor: 'Thai Sookdee',
+      eta: '25',
+      img: 'http://assets.cakebatterandbowl.com/wp-content/uploads/2013/03/wraps2.jpg',
+      favorite: false,
+      ingredients: ['lettuce', 'edamame', 'carrots', 'celery'],
+      fat: 1,
+      calories: 400,
+      salt: 3,
+      vegetarian: true,
+      thai: true,
+      italian: false
     }
   ];
-
-  $scope.history = [];
-
-  $scope.goBack = function(){    
-    console.log('back');   
-    $window.history.back();
-  };
-
-  // $scope.instructions = 'Click to add.';
-
-  // $scope.save = function() {
-  //   console.log($scope.instructions);
-  // }
 
   $scope.styles = [
     {
@@ -81,8 +125,19 @@ angular.module('NomWell.controllers.Main', [])
         $scope.styles[0].select = '0.3em solid white';
       }
     }
-  }
+  };
+
+  $scope.selectedFood = null;
+
+  $scope.select = function(food){
+    if ($scope.selectedFood !== food){
+      $scope.selectedFood = food;
+    } else {
+      $scope.selectedFood = null;
+    }
+  };
+
+  
 
 
 });
-
